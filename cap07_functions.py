c19c027092ffc075c47cf6f4f9ecac7b185e4ff3
fernando-cortes-tejada <fcortes@pucp.edu.pe>
2024-05-18 11:30:26 -0500

    clase 7 funciones

--- cap07_functions.py
+++ cap07_functions.py
@@ -1,2 +1,65 @@
-def function():
-    return "Hello from a function"
+def imprimir():
+    print("Fernando Cortés")
+    return 1
+
+
+x = imprimir()
+
+
+def imprimir_nombre(nombre: str) -> str:
+    print(nombre)
+    return nombre[0]
+
+
+x = imprimir_nombre("Fernando")
+
+
+def imprimir_nombre_completo(nombre: str, apellido: str) -> str:
+    print(nombre, apellido)
+    return nombre[0] + apellido[0]
+
+
+x = imprimir_nombre_completo(apellido="Cortés", nombre="Fernando")
+y = imprimir_nombre_completo("José", "Pérez")
+z = imprimir_nombre_completo("María", apellido="Gómez")
+w = imprimir_nombre_completo(apellido="García", nombre="Luis")
+
+
+def imprimir_familia_cortes(nombre: str, apellido: str = "Cortés") -> str:
+    print(nombre, apellido)
+    return nombre[0] + apellido[0]
+
+
+imprimir_familia_cortes("Fernando")
+imprimir_familia_cortes("Fernando", "Gómez")
+
+
+def suma_numeros(*numeros):
+    suma = 0
+    for numero in numeros:
+        suma += numero
+    return suma
+
+
+suma_numeros(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+
+def imprima_parametros_y_valores(**kwargs):
+    for parametro, valor in kwargs.items():
+        print(parametro, valor)
+
+
+imprima_parametros_y_valores(nombre="Fernando", apellido="Cortés", edad=30)
+
+
+def obtener_iniciales(nombre: str, apellido: str) -> str:
+    return nombre[0] + apellido[0]
+
+
+def imprimir_nombre_completo(nombre: str, apellido: str):
+    iniciales = obtener_iniciales(nombre, apellido)
+    print(iniciales, nombre, apellido)
+
+
+imprimir_nombre_completo("josé", "Pérez")
+iniciales = obtener_iniciales("Fernando", "Cortés")
--- web_scraping.py
+++ web_scraping.py
@@ -1,85 +1,114 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
-print("Here the sample test case will be started")
+import time
+
 driver = webdriver.Chrome()
-
-driver.get("https://listado.mercadolibre.com.pe/teclados")
-
-elementos = driver.find_elements(
-    by=By.CLASS_NAME,
-    value="ui-search-layout__item.shops__layout-item.ui-search-layout__stack",
-)
-
-len(elementos)
-
-elemento = elementos[0]
-
-ele_precio = elemento.find_element(
-    by=By.CLASS_NAME, value="andes-money-amount__fraction"
-)
-
-ele_precio.text
 
 datos = []
 dato = {}
-for elemento in elementos:
-    ele_titulo_url = elemento.find_element(
-        by=By.CLASS_NAME,
-        value="ui-search-item__group__element.ui-search-link__title-card.ui-search-link",
-    )
-    titulo = ele_titulo_url.get_attribute("title")
-    url = ele_titulo_url.get_attribute("href")
 
-    ele_precio = elemento.find_element(
-        by=By.CLASS_NAME, value="andes-money-amount__fraction"
+
+def get_precio(elemento: WebElement):
+    return float(
+        elemento.find_element(
+            by=By.CLASS_NAME, value="andes-money-amount__fraction"
+        ).text
     )
-    print(ele_precio.text)
+
+
+def get_descuento(elemento: WebElement):
     try:
-        ele_centavos = elemento.find_element(
-            by=By.CLASS_NAME,
-            value="andes-money-amount__cents.andes-money-amount__cents--superscript-24",
+        elemento_descuento = elemento.find_element(
+            by=By.CLASS_NAME, value="ui-search-price__discount"
         )
-        print(f"{ele_precio.text}.{ele_centavos.text}")
-    except Exception as e:
-        ele_centavos = None
+        desc_string = elemento_descuento.text
+        return float(desc_string.replace("% OFF", "")) / 100
+    except Exception:
+        return None
 
+
+def get_label(elemento: WebElement):
     try:
-        ele_descuento = elemento.find_element(
-            by=By.CLASS_NAME, value="ui-search-price__second-line"
-        ).find_element(by=By.CLASS_NAME, value="andes-money-amount__fraction")
-        print(ele_descuento.text)
-    except Exception as e:
-        ele_descuento = None
+        return elemento.find_element(
+            by=By.CLASS_NAME,
+            value="ui-search-item__highlight-label__container",
+        ).text
+    except Exception:
+        return None
 
-    dato = {
-        "precio_entero": ele_precio.text,
-        "precio_decimal": ele_centavos.text if ele_centavos else None,
-        "descuento": ele_descuento.text if ele_descuento else None,
-    }
 
-    # ahora append a todos los datos a la lista
-    datos.append(dato)
+def get_foto_url(elemento: WebElement):
+    try:
+        elemento_padre = elemento.find_element(
+            by=By.CLASS_NAME,
+            value="andes-carousel-snapped__slide.andes-carousel-snapped__slide--active",
+        )
+        elemento_foto = elemento_padre.find_element(
+            by=By.CLASS_NAME, value="ui-search-result-image__element"
+        )
+        return elemento_foto.get_attribute("src")
+    except Exception:
+        return None
+
+
+driver.get("https://listado.mercadolibre.com.pe/audifonos")
+for pagina in range(1, 2):
+    print(f"Pagina {pagina}")
+    time.sleep(3)
+    elementos = driver.find_elements(
+        by=By.CLASS_NAME,
+        value="ui-search-layout__item.shops__layout-item.ui-search-layout__stack",
+    )
+
+    print(f"Elementos en la pagina {pagina}: {len(elementos)}")
+    for elemento in elementos:
+        precio = get_precio(elemento)
+        porc_desc = get_descuento(elemento)
+        label = get_label(elemento)
+        foto_url = get_foto_url(elemento)
+
+        dato = {
+            "precio": precio,
+            "descuento": porc_desc,
+            "label": label,
+            "foto_url": foto_url,
+        }
+        datos.append(dato)
+
+    elemento_siguiente = driver.find_element(
+        by=By.CLASS_NAME,
+        value="andes-pagination__button.andes-pagination__button--next",
+    )
+    ele_sig = elemento_siguiente.find_element(
+        by=By.CLASS_NAME, value="andes-pagination__link"
+    )
+    siguiente_pagina_url = ele_sig.get_attribute("href")
+    driver.get(siguiente_pagina_url)
 
 
 import pandas as pd
 
 df = pd.DataFrame(datos)
 
+datos[0].get("foto_url")
+
+import requests
+
+img_data = requests.get(datos[0].get("foto_url")).content
+id_img1 = "audifonos_sony"
+with open(f"fotos/{id_img1}.jpg", "wb") as handler:
+    handler.write(img_data)
+
+df.label.value_counts()
+
+df[df["label"] == "MÁS VENDIDO"]
+
+~df["vendedor"].isna()
+
+
 driver.close()
 
 
-###########################
-# si quieren entrar a los comentarios de un producto
-# primero tienen que entrar a la url del producto
-
-# driver.get(url)
-
-# luego al elemento de comentarios
-# ele_coments = driver.find_elements(
-# by=By.CLASS_NAME, value="ui-review-capability-comments__comment"
-# )
-
-# y luego iterar sobre los comentarios
-# for ele_coment in ele_coments:
-#     ele_coment.text
+[elemento.get("url") for elemento in elementos]
